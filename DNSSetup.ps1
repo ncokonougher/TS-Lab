6621fd2243083d8a36723119ac73534da78a709f
ncokonougher <88517677+ncokonougher@users.noreply.github.com>
2022-12-07 19:16:31 -0500

    Add files via upload

--- DNSSetup.ps1
+++ DNSSetup.ps1
@@ -1,4 +1,5 @@
 # Obtains Ethernet ifIndex and stores it as a variable
 $Ethernet = (Get-NetAdapter -Name "Ethernet").ifIndex
 
+# Sets DNS Server using the $ethernet variable created above
 set-DNSClientServerAddress -InterfaceIndex $Ethernet -ServerAddresses ("10.0.2.15")
